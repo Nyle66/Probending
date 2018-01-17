c464144ce5e27f49a11efc472731a92a285b0b53
alsina jeremy <myname@hotmail.fr>
2018-01-17 10:53:05 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Accueil</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div class="center">
+
+                        <h1>Welcome in the first Pro-Bending Game 2.0</h1>
+                        <br><br>
+                        <button class="buttonAccueil"><a href="{{ url('/profil') }}">Go to Profil</a></button>
+                        <br><br>
+                        <button class="buttonAccueil"><a href="{{ url('/game') }}">Go to Play</a></button>
+                    
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
